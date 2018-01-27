f597aef3b55b8a10b2d754003dc9c7509b98ec00
Ezzeri Esa <esa@squareup.com>
2018-01-26 18:52:01 -0800

    added parallel broadcast and naive get/set

--- parallel/balancer.py
+++ parallel/balancer.py
@@ -3,10 +3,11 @@ import socket
 import sys
 import thread
 from datetime import datetime as dt
+from Queue import Queue
 
 sys.path.insert(0, "..")
-from helpers.bencode import encode_bencode
-from helpers.connect import parse_arguments, bind_socket
+from helpers.bencode import encode_bencode, decode_bencode
+from helpers.connect import parse_arguments, bind_socket, dispatch_status
 
 
 BALANCER_IP = os.getenv('BALANCER_IP_ADDRESS')
@@ -18,40 +19,114 @@ class Balancer(object):
         self.sock = sock
         self.source = source
         self.targets = targets
+        self.data = {}
         self.counter = 0
-        self.exit = False
+        self.end = False
+
+    def deliver(self, message, address):
+        self.sock.sendto(message, address)
+        self.counter += 1
+
+    def broadcast(self, message):
+        for target in self.targets:
+            address = (NODE_IP, target)
+            self.deliver(message, address)
+
+    def status(self):
+        payload = ['request', 'status', str(self.counter)]
+        message = encode_bencode(payload)
+        self.broadcast(message)
+
+    def reset(self):
+        payload = ['request', 'reset', str(self.counter)]
+        message = encode_bencode(payload)
+        self.broadcast(message)
+
+    def exit(self):
+        payload = ['request', 'exit', str(self.counter)]
+        message = encode_bencode(payload)
+        self.broadcast(message)
+
+        self.end = True
+        sys.exit(0)
+
+    def choose(self):
+        return self.targets[self.counter % len(self.targets)]
+
+    def get(self, command, send=True):
+        target = self.choose()
+        if os.getenv('BALANCER_DEBUG'):
+            dispatch_status('get', 'request', 'to', target)
+
+        payload = ['request', 'get', self.counter, 'key', command[1]]
+        message = encode_bencode(payload)
+
+        if send:
+            address = (NODE_IP, target)
+            self.deliver(message, address)
+
+        return message
+
+    def set(self, command, send=True):
+        target = self.choose()
+        if os.getenv('BALANCER_DEBUG'):
+            dispatch_status('set', 'request', 'to', target)
+
+        payload = ['request', 'set', self.counter, 'key', command[1], 'value', command[2]]
+        message = encode_bencode(payload)
+
+        if send:
+            address = (NODE_IP, target)
+            self.deliver(message, address)
+
+        return message
 
     def execute(self, command):
-        sys.stderr.write(str(command) + '\n')
+        if command[0] == 'status':
+            if os.getenv('BALANCER_DEBUG'):
+                sys.stderr.write(str(dt.now()) + ' INFO system status\n')
+            self.status()
+            sys.stderr.write(str(self.data) + '\n')
+
+        elif command[0] == 'reset':
+            if os.getenv('BALANCER_DEBUG'):
+                sys.stderr.write(str(dt.now()) + ' WARN system reset\n')
+            self.reset()
+
+        elif command[0] == 'exit':
+            if os.getenv('BALANCER_DEBUG'):
+                sys.stderr.write(str(dt.now()) + ' WARN system exit\n')
+            self.exit()
+
+        elif command[0] == 'get':
+            self.get(command)
+
+        elif command[0] == 'set':
+            self.set(command)
 
     def listen(self):
         while True:
             try:
                 sock.settimeout(3)
                 response, address = sock.recvfrom(1024)
-                payload = decode_bencode(request)
+                result = decode_bencode(response)
+
+                if result[1] == 'get' and result[6]:
+                    self.data[result[4]] = result[6]
 
             except socket.timeout:
-                pass
-
-            if self.exit:
-                thread.exit()
-
-            self.counter += 1
+                if self.end:
+                    thread.exit()
 
     def read(self):
         while True:
             command = raw_input('> ').split()
 
-            if command[0] == 'status':
-                sys.stderr.write(str(self.counter) + '\n')
+            if command[0] == 'get' and len(command) != 2:
+                sys.stderr.write('get requests requires only a key!\n')
 
-            elif command[0] == 'exit':
-                if os.getenv('BALANCER_DEBUG'):
-                    sys.stderr.write(str(dt.now()) + ' WARN system exit\n')
-
-                self.exit = True
-                sys.exit(0)
+            elif command[0] == 'set' and len(command) != 3:
+                sys.stderr.write('get requests requires only a key and a value!\n')
 
             self.execute(command)
 
--- parallel/node.py
+++ parallel/node.py
@@ -2,49 +2,113 @@ import os
 import socket
 import sys
 import thread
+from datetime import datetime as dt
+from Queue import Queue
 
 sys.path.insert(0, "..")
-from helpers.bencode import decode_bencode
-from helpers.connect import parse_arguments, bind_socket
+from helpers.bencode import encode_bencode, decode_bencode
+from helpers.connect import parse_arguments, bind_socket, dispatch_status
+from helpers.hashqueue import HashQueue
 
 
 NODE_IP = os.getenv('NODE_IP_ADDRESS')
 
 
-source, peers = parse_arguments()
-sock = bind_socket(NODE_IP, source)
+class Node(object):
+    def __init__(self, sock, source, peers):
+        self.sock = sock
+        self.source = source
+        self.peers = peers
+        self.data = {}
+        self.ingress = Queue(maxsize=100)
+        self.egress = HashQueue(maxsize=100)
+        self.end = False
 
-stack = []
+    def status(self):
+        sys.stderr.write(str(dt.now()) + ' INFO contents start\n')
+        for key, value in self.data.iteritems():
+            sys.stderr.write(key + ': ' + value + '\n')
+
+        sys.stderr.write(str(dt.now()) + ' INFO contents end\n')
+
+    def reset(self):
+        sys.stderr.write(str(dt.now()) + ' WARN system reset\n')
+        self.data = {}
+
+    def exit(self):
+        sys.stderr.write(str(dt.now()) + ' WARN system reset\n')
+        self.end = True
+        sys.exit(0)
+
+    def deliver(self, message, address, identifier):
+        if identifier is not None:
+            self.egress.put(identifier, (message, dt.now()))
+
+        self.sock.sendto(message, address)
+
+    def get(self, payload, address):
+        value = self.data.get(payload[4], '')
+        result = ['response', 'get', self.egress.status(), 'key', payload[4], 'value', value]
+
+        message = encode_bencode(result)
+        self.deliver(message, address, payload[2])
+
+        dispatch_status('get', 'response', 'to', address[1])
+
+    def set(self, payload, address):
+        self.data[payload[4]] = payload[6]
+        result = ['response', 'set', self.egress.status(), 'key', payload[4], 'value', payload[6]]
+
+        message = encode_bencode(result)
+        self.deliver(message, address, payload[2])
+
+        dispatch_status('set', 'response', 'to', address[1])
+
+    def listen(self):
+        while True:
+            try:
+                sock.settimeout(3)
+                request, address = sock.recvfrom(1024)
+
+                payload = decode_bencode(request)
+                self.ingress.put((payload, address))
+
+            except socket.timeout:
+                sys.stderr.write('.\n')
+
+                if self.end:
+                    thread.exit()
+
+    def execute(self):
+        while True:
+            if not self.ingress.empty():
+                payload, address = self.ingress.get()
+
+                if payload[1] == 'status':
+                    dispatch_status('status', 'request', 'from', address[1])
+                    self.status()
+
+                elif payload[1] == 'reset':
+                    dispatch_status('reset', 'request', 'from', address[1])
+                    self.reset()
+
+                elif payload[1] == 'exit':
+                    dispatch_status('exit', 'request', 'from', address[1])
+                    self.exit()
+
+                elif payload[1] == 'get':
+                    dispatch_status('get', 'request', 'from', address[1])
+                    self.get(payload, address)
+
+                elif payload[1] == 'set':
+                    dispatch_status('set', 'request', 'from', address[1])
+                    self.set(payload, address)
 
 
-def listen():
-    counter = 0
+if __name__ == '__main__':
+    source, peers = parse_arguments()
+    sock = bind_socket(NODE_IP, source)
 
-    while True:
-        try:
-            sock.settimeout(3)
-            request, address = sock.recvfrom(1024)
-
-            payload = decode_bencode(request)
-            stack.append(payload)
-
-            if payload[0] == 'exit':
-                sys.exit(0)
-
-        except socket.timeout:
-            sys.stderr.write('.\n')
-
-
-def execute():
-    while True:
-        if stack:
-            payload = stack.pop()
-
-            if payload[0] == 'exit':
-                sys.exit(0)
-
-            print str(payload)
-
-
-thread.start_new_thread(listen, ())
-execute()
+    node = Node(sock, source, peers)
+    thread.start_new_thread(node.listen, ())
+    node.execute()
